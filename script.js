cf7d6b9f0dee84f541dd6fd20a4a8b1d53300cfd
Debasis Nayak <115566899+debasisnayak12@users.noreply.github.com>
2023-11-12 10:29:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 //your JS code here. If required.
+const counterElem = document.getElementById('counter');
+const incrementElem = document.getElementById('incrementBtn');
+
+let counterValue = 0;
+
+const updateCounter = () => {
+	counterElem.textContent = counterValue;
+};
+
+const incCounter = () => {
+	alert(`un-incremented value: ${counterValue}`);
+	
+	counterValue++;
+	updateCounter();
+};
+
+incrementElem.addEventListener('click',incCounter);
+
